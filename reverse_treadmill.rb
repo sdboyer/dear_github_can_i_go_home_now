a54ca28e7bfa748ac8b263563db96806c01eacc3
Sam Boyer <tech@samboyer.org>
2014-03-12 12:32:35 -0400

    aaaaand rickroll

--- reverse_treadmill.rb
+++ reverse_treadmill.rb
@@ -6,12 +6,14 @@ messages = [
   'Sorry honey, I need to keep my streak going - we\'ll have sex tomorrow!'
 ]
 
-gh_founding = DateTime.new(2008,7,21)
-system({'GIT_COMMITTER_DATE' => gh_founding.to_date.rfc2822, 'GIT_AUTHOR_DATE' => gh_founding.to_date.rfc2822}, "env")
+rr = %w{ Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you\n }
 
+gh_founding = DateTime.new(2008,7,21)
+
+i = 0
 gh_founding.to_date.step(Date.today) do |day|
-  File.open('self_worth', 'a') { |f| f.write("1\n") }
+  File.open('self_worth', 'a') { |f| f.write(rr[i % 34]) }
   File.open('msg', 'w') { |f| f.write(messages.sample) }
-  system({'GIT_COMMITTER_DATE' => day.rfc2822, 'GIT_AUTHOR_DATE' => day.rfc2822}, "git commit -F msg -- self_worth")
+  system({'GIT_COMMITTER_DATE' => day.rfc2822, 'GIT_AUTHOR_DATE' => day.rfc2822}, "git commit -q -F msg -- self_worth")
 end
 
