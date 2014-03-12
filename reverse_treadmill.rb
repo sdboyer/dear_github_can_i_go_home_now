87702c2c2f6707b6665c25039c5590b361cd00d6
Sam Boyer <tech@samboyer.org>
2014-03-12 11:52:49 -0400

    Actually write to the file

--- reverse_treadmill.rb
+++ reverse_treadmill.rb
@@ -9,6 +9,7 @@ messages = [
 gh_founding = DateTime.new(2008,7,21)
 
 gh_founding.to_date.step(Date.today) do |day|
+  File.open('my_value_as_a_coder', 'a') { |f| f.write('1') }
   `GIT_COMMITTER_DATE=#{day.rfc2822} GIT_AUTHOR_DATE=#{day.rfc2822} git commit my_value_as_a_coder -m '#{messages.sample}'`
 end
 
