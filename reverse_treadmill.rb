566281afff0e7346ce684dbe2b0d5c9504aef808
Sam Boyer <tech@samboyer.org>
2014-03-12 12:25:25 -0400

    stop the insanity of trying to escape a sentential shell arg

--- reverse_treadmill.rb
+++ reverse_treadmill.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby -w
 require 'date'
-require 'shellwords'
 
 messages = [
   'My kid\'s appointment at the pediatrician can wait, I need to get this commit in.',
@@ -8,10 +7,11 @@ messages = [
 ]
 
 gh_founding = DateTime.new(2008,7,21)
+system({'GIT_COMMITTER_DATE' => gh_founding.to_date.rfc2822, 'GIT_AUTHOR_DATE' => gh_founding.to_date.rfc2822}, "env")
 
 gh_founding.to_date.step(Date.today) do |day|
   File.open('self_worth', 'a') { |f| f.write("1\n") }
-  system({'GIT_COMMITTER_DATE' => day.rfc2822, 'GIT_AUTHOR_DATE' => day.rfc2822}, "git commit -m '#{Shellwords.join(messages.sample.split(' '))}' self_worth")
+  File.open('msg', 'w') { |f| f.write(messages.sample) }
+  system({'GIT_COMMITTER_DATE' => day.rfc2822, 'GIT_AUTHOR_DATE' => day.rfc2822}, "git commit -F msg -- self_worth")
 end
-
 
